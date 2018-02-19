4ed6ba6dfecfe62eab87e2a329d801b647891c0a
Joshua.Duncan <jd52981@gmail.com>
2018-02-18 22:08:41 -0800

    Add files via upload

---
+++ Change.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+
+
+public class Change {
+    
+
+	private static int getChange(int m) {
+
+		int remainder = 0;
+		int pennies = ((m%10)%5);
+		remainder = m - pennies;
+		int dimes = (remainder - (remainder%10))/10;
+		remainder = remainder - (dimes*10);
+		int nickels = (remainder - (remainder%5))/5;  
+		      
+		//write your code here
+        
+		return (dimes + nickels + pennies);
+    
+	}
+
+    
+
+	public static void main(String[] args) {
+        
+		Scanner scanner = new Scanner(System.in);
+        
+		int m = scanner.nextInt();
+        
+		System.out.println(getChange(m));
+
+    
+	}
+
+}
+
---
+++ DifferentSummands.java
@@ -0,0 +1,60 @@
+import java.util.*;
+
+
+
+public class DifferentSummands {
+    
+
+	private static List<Integer> optimalSummands(int n) {
+        
+		List<Integer> summands = new ArrayList<Integer>();
+        
+		//write your code here
+        
+		int compare = 0;
+		int i = 1;
+		int lastAccess = 0;
+		while( compare < n ) {
+			compare = compare + i;
+			if( compare <= n) {
+				summands.add(i);
+			} else if ( compare > n && lastAccess == 0 ) {
+				summands.remove(i - 2);
+				summands.add(i);
+				lastAccess = i - 2;
+				compare = (compare-i) + 1;
+			} else if ( compare > n && lastAccess != 0 ) {
+				summands.remove(lastAccess);
+				summands.add(i);
+				//lastAccess = lastAccess +1;
+				compare = (compare-i) + 1;
+			}
+			i++;
+		}
+		return summands;
+    
+	}
+    
+    
+
+	public static void main(String[] args) {
+        
+		Scanner scanner = new Scanner(System.in);
+        
+		int n = scanner.nextInt();
+        
+		List<Integer> summands = optimalSummands(n);
+        
+		System.out.println(summands.size());
+    
+    
+		for (Integer summand : summands) {
+            
+			System.out.print(summand + " ");
+        
+		}
+    
+	}
+
+}
+
