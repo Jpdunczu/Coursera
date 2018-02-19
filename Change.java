40e076c8c11763a4f8bfe3f49a6139cadceebf00
Joshua.Duncan <jd52981@gmail.com>
2018-02-18 22:15:18 -0800

    Update Change.java

--- Change.java
+++ Change.java
@@ -3,7 +3,7 @@
 Task. The goal in this problem is to find the minimum number of coins needed to change the input value
 (an integer) into coins with denominations 1, 5, and 10.
 Input Format. The input consists of a single integer 𝑚.
-Constraints. 1 ≤ 𝑚 ≤ 103.
+Constraints. 1 ≤ 𝑚 ≤ 10^3.
 Output Format. Output the minimum number of coins with denominations 1, 5, 10 that changes 𝑚.
 */
 import java.util.Scanner;
